57f7359c3934805e73198182b66a76954ed61b00
Chris <dietrichc@cip.ifi.lmu.de>
2024-09-25 22:26:30 +0200

    disable scrolling on homepage

--- javascript/home.js
+++ javascript/home.js
@@ -1,3 +1,6 @@
 window.onload = function () {
     window.scrollTo(0, 0);
 }
+window.addEventListener('scroll', function() {
+    window.scrollTo(0, 0); // Setzt die Scroll-Position immer wieder auf 0
+});
