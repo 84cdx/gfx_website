5b43e1ccaeeb7e187d0032555320744159159d62
Chris <dietrichc@cip.ifi.lmu.de>
2024-10-02 07:41:13 +0200

    add transitions for 'about' and 'contact' pages

---
+++ javascript/about.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    var elements = document.querySelectorAll('.fade-in');
+
+    elements.forEach(function (element) {
+        setTimeout(function () {
+            element.classList.add('visible');
+        }, 50);
+    });
+});
